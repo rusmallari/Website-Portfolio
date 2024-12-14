f1aa8bce8edf15efd5a4c64e9031457f09898bae
rusmallari <143236365+rusmallari@users.noreply.github.com>
2024-12-14 12:39:42 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.querySelector('.hamburg');
     const cancel = document.querySelector('.cancel');
     const dropdown = document.querySelector('.dropdown');
-    
+
     hamburger.addEventListener('click', function() {
-        dropdown.style.display = 'block'; // Show the dropdown
+        dropdown.style.transform = 'translateY(0px)'; // Show the dropdown
         hamburger.style.opacity = '0'; // Hide hamburger
         cancel.style.opacity = '1'; // Show cancel button
-        cancel.style.pointerEvents = 'visible'; // Enable cancel button
+        cancel.style.pointerEvents = 'auto'; // Enable cancel button
     });
 
     cancel.addEventListener('click', function() {
-        dropdown.style.display = 'none'; // Hide the dropdown
+        dropdown.style.transform = 'translateY(-500px)'; // Hide the dropdown
         hamburger.style.opacity = '1'; // Show hamburger again
         cancel.style.opacity = '0'; // Hide cancel button
         cancel.style.pointerEvents = 'none'; // Disable cancel button
